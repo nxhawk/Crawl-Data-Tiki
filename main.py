257289389b794f6770573441ab888ac572437052
haonhat0703 <haonhat2729@gmail.com>
2023-03-01 22:01:52 +0700

    crawl data simple => store data/[filename].txt

---
+++ main.py
@@ -0,0 +1,70 @@
+import json
+import os
+import re
+from bs4 import BeautifulSoup
+import requests
+
+from dotenv import load_dotenv
+load_dotenv()
+
+TIKI = os.environ.get("TIKI")
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"}
+res = requests.get(TIKI, headers=headers)  # must have headers
+soup = BeautifulSoup(res.text, 'html.parser')  # get main source
+
+# listTitle[]
+# ---->title, link, more[name, link]
+listTitle = []  # contain title
+for ahref in soup.find_all('div', class_="styles__StyledCategory-sc-17y817k-1 iBByno"):
+    item = {"title": ahref.find('a').get_text()}
+    item.update({"link": ahref.find('a').attrs['href']})
+    listMore = []
+    for bhref in ahref.find('p').find_all('a'):
+        listMore.append({"name": bhref.get_text(),
+                        "link": bhref.attrs['href']})
+    item.update({"more": listMore})
+    listTitle.append(item)
+
+
+id_product = []
+
+# infor one
+# https://tiki.vn/api/v2/products/{id}
+
+
+def _writeData(f, ite):
+    id_product = []
+    page = "https://tiki.vn/api/personalish/v1/blocks/listings?limit=40&include=advertisement&aggregations=2&trackity_id=fb703a37-2f3d-2956-96ac-bd90536bb792&category={}&page=".format(
+        re.sub(r'\D', '', ite['link']))
+    print()
+    print(page)
+    num = 1
+    while True:
+        print('Process "{}" page {}'.format(ite['name'], num))
+        result = requests.get(page+str(num), headers=headers).text
+        if (json.loads(result).get('data') == None or json.loads(result).get('data') == []):
+            break
+        for item in json.loads(result).get('data'):
+            if item.get('id') not in id_product:
+                id_product.append(item.get('id'))
+                f.write(str(item.get('id'))+'\n')
+                f.write(item.get('name')+'\n')
+                f.write(str(item.get('price'))+'\n')
+                f.write(item.get("thumbnail_url")+'\n\n')
+        num += 1
+
+
+for title in listTitle:
+    print('\nProcess "{}"'.format(title['title']))
+    print('-'*30)
+    file = 'data/' + title['title'] + '.txt'
+    f = open(file, 'w', encoding="utf-8")
+    for item in title['more']:
+        f.write('[NEW] '+item['name']+'\n\n')
+        _writeData(f, item)
+
+    f.close()
+    print('DONE!!!!!!!!!!!!!!!!')
+    print('-'*30)
